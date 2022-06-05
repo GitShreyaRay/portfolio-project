4eb671e4fd99c5a55c4884d9fd1bc22df936bdf2
GitShreyaRay <marsvsjupiter@gmail.com>
2022-06-05 16:33:50 +0530

    Added Blog details.

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,15 @@ class Blog(models.Model):
   pub_date = models.DateTimeField(auto_now=True)
   body = models.TextField()
   image = models.ImageField(upload_to='images/')
+
+  def __str__(self):
+      return self.title
+
+  def summary(self):
+      return self.body[:100] + "..."
+
+  def pub_date_pretty(self):
+      return self.pub_date.strftime('%b %e %Y')
 # Add the Blog app to the settings
 
 # create a migration
